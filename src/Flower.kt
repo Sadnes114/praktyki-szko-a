1ca2a86d095505ce46ecb94f15697e013ac5f3cf
Sadnes <czerwikdawid08@gmail.com>
2025-11-02 11:44:08 +0100

    uzupelnienie

--- src/Flower.kt
+++ src/Flower.kt
@@ -1,2 +1,11 @@
-class Flower {
+class Flower(name: String): Plant(name) {
+    private var petal: Int = 0
+    fun addPetal() {
+        petal += 1
+        println("Petal: $petal")
+    }
+    fun deletePetal() {
+        petal -= 1
+        println("Petal: $petal")
+    }
 }
